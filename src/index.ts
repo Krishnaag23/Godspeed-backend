7ef7d255a53fc1f78f858af69383987acaa357af
Love2104 <Lovechourasia2004@gmail.com>
2025-05-16 20:25:09 +0530

    server

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,6 @@ try {
     console.error(error);
     process.exit(1);
 }
-
 import Godspeed from "@godspeedsystems/core";
-
-// create a godspeed
 const gsApp = new Godspeed();
-
-// initilize the Godspeed App
-// this is responsible to load all kind of entities
 gsApp.initialize();
